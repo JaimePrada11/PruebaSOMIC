34a6820498878fda96ef854733077cc722e6e58d
JaimePrada11 <jandrespradato.11@gmail.com>
2025-04-19 11:30:23 -0500

    feat: :sparkles: Actualización de los modelos

--- Backend/src/models/Articulo.js
+++ Backend/src/models/Articulo.js
@@ -5,10 +5,10 @@ const Articulo = sequelize.define('Articulo', {
     IDArticulo: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     ArticuloNombre: { type: DataTypes.STRING(100), allowNull: false },
     ArticuloLaboratorio: { type: DataTypes.STRING, allowNull: false },
-    ArticuloSaldo: { type: DataTypes.DECIMAL(10, 2), allowNull: false },
+    ArticuloSaldo: { type: DataTypes.INTEGER, allowNull: false },
     ArticuloCostos: { type: DataTypes.DECIMAL(10, 2), allowNull: false },
     ArticuloPrecioVenta: { type: DataTypes.DECIMAL(10, 2), allowNull: false },
-
+    
 }, {
     timestamps: false
 
--- Backend/src/models/FacturaKardex.js
+++ Backend/src/models/FacturaKardex.js
@@ -8,8 +8,8 @@ const FacturaKardex = sequelize.define('FacturaKardex', {
     IDFacturaKardex: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     IDFactura: { type: DataTypes.INTEGER, allowNull: false },
     IDArticulo: { type: DataTypes.INTEGER, allowNull: false },
-    FacturaKardexCantidad: { type: DataTypes.DECIMAL(10, 2), allowNull: false},
-    FacturaKardexnaturaleza: { type: DataTypes.ENUM('+', '-'), allowNull: false },
+    FacturaKardexCantidad: { type: DataTypes.INTEGER, allowNull: false},
+    FacturaKardexnaturaleza: { type: DataTypes.STRING(1), allowNull: false },
 }, {
     tableName: 'FacturaKardex',
     timestamps: false
